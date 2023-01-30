ff2760dd56acf6145ec43779d303e1bfc0cfcf89
Ellinor <ellinor.bakhuizen@gmail.com>
2023-01-30 21:00:15 +0100

    first api endpoint

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const PORT = process.env.port || 3001;
 const app = express();
 
+app.get("/api", (req, res) => {
+    res.json({ message: "This is your server speaking!" });
+  });
+
 app.listen(PORT, () => {
     console.log("Server is listening on port", PORT)
 });
