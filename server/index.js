26718656dbd9edaaa06966998e4c367d97393672
Ellinor <ellinor.bakhuizen@gmail.com>
2023-02-02 18:52:52 +0100

    use rss-parser library to parse rss feed

--- server/index.js
+++ server/index.js
@@ -1,6 +1,22 @@
-const express = require("express");
+import express from 'express';
+import RSSParser from "rss-parser";
+
 const PORT = process.env.port || 3001;
 const app = express();
+
+const rssFeedUrl = "https://www.svt.se/rss.xml";
+
+const parse = async url => {
+  const feed = await new RSSParser().parseURL(url);
+
+  console.log(feed.title);
+
+  feed.items.forEach(item => {
+    console.log(`${item.title} - ${item.link}\n${item.contentSnippet}\n\n`);
+  });
+}
+
+parse(rssFeedUrl);
 
 app.get("/api", (req, res) => {
     res.json({ message: "This is your server speaking!" });
