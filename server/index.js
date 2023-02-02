0e8ea849331c9a6526985c5b37c6389b85b1a8b3
Ellinor <ellinor.bakhuizen@gmail.com>
2023-02-02 21:05:52 +0100

    cleanup server index file

--- server/index.js
+++ server/index.js
@@ -4,21 +4,13 @@ import RSSParser from "rss-parser";
 const PORT = process.env.port || 3001;
 const app = express();
 
-const rssFeedUrl = "https://www.svt.se/rss.xml";
-
-const parseAndTransform = async url => {
-  const feed = await new RSSParser().parseURL(url);
-
-  const transformedResponse = feed.items.map(item => ({title: item.title, contentSnippet: item.contentSnippet}));
-  return transformedResponse;
-}
-
 app.use(express.json())
 
 app.post("/api", (req, res) => {
   (async () => {
-    const list = await parseAndTransform(req.body.url)
-    res.json({ message: list });
+    const rssFeed = await new RSSParser().parseURL(req.body.url);
+    const transformedResponse = rssFeed.items.map(item => ({title: item.title, contentSnippet: item.contentSnippet}));
+    res.json({ message: transformedResponse });
   })()
     
   });
