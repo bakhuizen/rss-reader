d98800dc84cea2f359084ac4e6e857486c35e6a3
Ellinor <ellinor.bakhuizen@gmail.com>
2023-02-03 21:43:33 +0100

    let user submit rss link, use wordclout library

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,52 @@
-import {useEffect, useState} from 'react';
+import {useState} from 'react';
+import ReactWordcloud from 'react-wordcloud';
 import './App.css';
 
 function App() {
   const [data, setData] = useState(null);
 
-  const requestData = { url: "https://www.svt.se/rss.xml" };
+  const [url, setUrl] = useState("https://www.svt.se/rss.xml");
+  const [words, setWords] = useState([{text: 'hello', value: 64,}])
+  /*
+  https://www.svt.se/rss.xml
+  https://www.dn.se/rss/
+  */
 
-  useEffect(() => {
+  const fetchRssfeed = (rssLink) => {
     fetch("/api", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(requestData),
-    })
-      .then((result) => result.json())
+      body: JSON.stringify({ url: rssLink }),
+    }).then((result) => result.json())
       .then((data) => setData(data.message));
-  })
+  }
+
+  const transformData = (list) => {
+    return list?.map(item => ({text: item?.contentSnippet || '', value: 20}))
+  }
+
+  const handleButtonClick = () => {
+    fetchRssfeed(url)
+    if(data){
+      const newWords = transformData(data);
+      console.log(newWords);
+      setWords(newWords);
+    }
+  }
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          {!data ? "Loading..." : data}
-        </p>
+        <div style={{display:'flex', flexDirection:'column'}}>
+          <div>
+            <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} />
+            <button onClick={() => handleButtonClick()}>Click here</button>
+          </div>
+          {data && <ReactWordcloud words={words}/>}
+        </div>
       </header>
     </div>
   );
