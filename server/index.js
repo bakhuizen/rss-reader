3584b1b3dbff0bf8e69a18e837470086c84401a7
Ellinor <ellinor.bakhuizen@gmail.com>
2023-02-05 13:01:52 +0100

    transform rss feed to word frequency map

--- server/index.js
+++ server/index.js
@@ -6,9 +6,31 @@ const app = express();
 
 app.use(express.json())
 
-app.post("/api", (req, res) => {
+const transformResponse = (data) => {
+  const initialValue = "";
+  const allText = data.items.reduce(
+    (accumulator, currentItem) => 
+      accumulator + " " + currentItem.title + " " + currentItem.contentSnippet,
+    initialValue
+  );
+
+  const words = allText.toLowerCase().replace(/[^åäöáéa-z\s]/gi, ' ').split(/\s/);
+
+  const frequencyMap = {};
+  words.forEach((word) => {
+    if(!frequencyMap[word]){
+      frequencyMap[word] = 0;
+    }
+    frequencyMap[word] += 1;
+  })
+
+  console.log(frequencyMap);
+}
+
+app.post("/api/rss-feed/words", (req, res) => {
   (async () => {
     const rssFeed = await new RSSParser().parseURL(req.body.url);
+    transformResponse(rssFeed);
     const transformedResponse = rssFeed.items.map(item => ({title: item.title, contentSnippet: item.contentSnippet}));
     res.json({ message: transformedResponse });
   })()
